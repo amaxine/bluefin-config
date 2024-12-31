60a73a4c8485059871b91bb3725c00e158dcc93f
Maxine Aubrey <max@ine.dev>
2024-12-31 18:39:36 +0000

    test

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen firefox tmux vim gnome-terminal
+rpm-ostree install screen firefox tmux vim gnome-terminal fish
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
